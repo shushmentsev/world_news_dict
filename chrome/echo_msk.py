b8d510b06fccf29a880ec26e14f77cb7e13626b7
Shushmencev Anton <shushmencev97@mail.ru>
2020-02-21 17:38:17 +0700

    Get URLs

--- chrome/echo_msk.py
+++ chrome/echo_msk.py
@@ -13,54 +13,91 @@ driver_path = "D:/Anton Shushmencev/19.02.20/progs/chrome_driver/chromedriver.ex
 options = Options()
 
 #Получение драйвера:
-driver = webdriver.Chrome( \
-        options = options, \
-        executable_path = driver_path, \
-        )
+def create_driver():
+    driver = webdriver.Chrome( \
+            options = options, \
+            executable_path = driver_path, \
+            )
 
-#Максимальный размер окна браузера:
-driver.maximize_window()
+    return driver
 
-#Безголовый режим:
+def get_urls(url):
+    driver.get(url)
 
-#Переход по ссылке. Программа "Код доступа":
-driver.get("https://echo.msk.ru/programs/code/")
+    #Задержка, чтобы страница успела прогрузиться:
+    time.sleep(5)
 
-#Получение ссылок на статьи:
-my_list = driver.find_elements_by_css_selector("#archive > div.rel > div > div > div.mediamenu > a.watch.iblock")
+    #Получение ссылок на статьи:
+    my_list = driver.find_elements_by_css_selector("#archive > div.rel > div > div > div.mediamenu > a.watch.iblock")
 
-for i in range(len(my_list)):
-    print(my_list[i])
+    page_urls = []
+    #Вывод полученных ссылок:
+    for i in range(len(my_list)):
+        page_urls.append(my_list[i].get_attribute("href"))
 
-for i in range(len(my_list)):
-    print(my_list[i].get_attribute("href"))
+    return page_urls
 
-#Переход по ссылке:
-driver.get(my_list[0].get_attribute("href"))
+def get_text():
+    
+    #Получение текста передачи:
+    continue_flag = True
+    my_list = []
+    while continue_flag:
+        try:
+            #Получение текста передачи:
+            my_list = driver.find_elements_by_css_selector("#mmread > div > div > p")
+            print("Спарсил текст")
 
-#Получение текста передачи:
-continue_flag = True
-my_list = []
-while continue_flag:
-    try:
-        #Получение текста передачи:
-        my_list = driver.find_elements_by_css_selector("#mmread > div > div > p")
-        print("Спарсил текст")
+        except:
+            print("Исключение")
 
-    except:
-        print("Исключение")
+        else:
+            continue_flag = False
 
-    else:
-        continue_flag = False
+    #Запись текста передачи в файл:
+    f = open("text.txt", "w", encoding = "utf-8")
+    for i in range(len(my_list)):
+        f.write(my_list[i].text)
 
-#Запись текста передачи в файл:
-f = open("text.txt", "w", encoding = "utf-8")
-for i in range(len(my_list)):
-    f.write(my_list[i].text)
-
-f.close()
+    f.close()
 
 #Получение ссылки на видео:
-i_frame = driver.find_element_by_css_selector("#mmread > div > iframe")
-yt_url = i_frame.get_attribute("src")
-print("Ссылка на видео: ", yt_url)
+def get_yt_url():
+    i_frame = driver.find_element_by_css_selector("#mmread > div > iframe")
+    yt_url = i_frame.get_attribute("src")
+    print("Ссылка на видео: ", yt_url)
+
+if __name__ == "__main__":
+    driver = create_driver()
+    
+    #Максимальный размер окна браузера:
+    driver.maximize_window()
+
+    #Безголовый режим:
+
+    
+    #Получение ссылок на страницы:
+    pages = []
+
+    #Открытие файла "urls.txt":
+    f = open("urls.txt", "w", encoding = "utf-8")
+    
+    for i in range(40):
+        url = "https://echo.msk.ru/programs/code/archive/" + str(i + 1) + "/"
+        print("Код доступа. Страница " + str(i + 1) + ":")
+        urls = get_urls(url)
+
+        #Запись текста передачи в файл:
+        for i in range(len(urls)):
+            f.write(urls[i] + "\n")
+        
+        
+        #Вывод ссылок на страницы:
+        for i in range(len(urls)):
+            print(urls[i])
+
+        print("\n")
+
+    #Закрытие файла "urls.txt":
+    f.close()
+        
